b579c11b7759c69ccd0371a97b1890ada2fa1a15
Anastasios Lelakis <tasos@hotmail.com>
2021-02-05 21:01:52 +0200

    add HibernateUtil

--- src/main/java/emergon/service/CustomerService.java
+++ src/main/java/emergon/service/CustomerService.java
@@ -1,51 +1,39 @@
 package emergon.service;
 
 import emergon.entity.Customer;
-import java.util.ArrayList;
+import emergon.repository.CustomerRepo;
 import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+@Transactional
 @Service
 public class CustomerService {
 
+    @Autowired
+    CustomerRepo customerRepo;
+    
     private List<Customer> customers;
 
     public List<Customer> getCustomers() {
-        if (customers == null) {
-            customers = new ArrayList();
-            customers.add(new Customer(1, "Jack"));
-            customers.add(new Customer(2, "Jonathan"));
-            customers.add(new Customer(3, "Jenny"));
-        }
+        List<Customer> customers = customerRepo.findAll();
         return customers;
     }
 
     public void addCustomer(Customer customer) {
-        customers.add(customer);
+        customerRepo.save(customer);
     }
 
     public void deleteCustomer(int id) {
-        for(Customer c: customers){
-            if(c.getCcode().equals(id)){
-                customers.remove(c);
-                break;
-            }
-        }
+        customerRepo.delete(Customer.class, id);
     }
 
     public Customer getCustomerById(int ccode) {
-        for(Customer c: customers){
-            if(c.getCcode() == ccode){
-                return c;
-            }
-        }
-        return null;
+        return customerRepo.find(ccode);
     }
 
     public Customer updateCustomer(Customer customer) {//customer argument contains the new data from the form
-        //Find customer from list and add it to the variable customerToUpdate
-        Customer customerToUpdate = getCustomerById(customer.getCcode());
-        customerToUpdate.setCname(customer.getCname());
-        return customerToUpdate;
+        return customerRepo.save(customer);
     }
 }
